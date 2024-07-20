7d982f2c9ef749938226044467b9822f20909416
TheMadRatter <50557502+TheMadRatter@users.noreply.github.com>
2024-07-19 22:29:31 -0400

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -1,21 +1,11 @@
+Here is my PHP:
+
 <?php
 // Enable error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Verify reCAPTCHA v3
-    $recaptcha_secret = "6LeVNhQqAAAAANtIxftQNWdZI_Xb4AC1iMvd3Ksp";
-    $recaptcha_response = $_POST['g-recaptcha-response'];
-
-    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response");
-    $responseKeys = json_decode($response, true);
-
-    if (intval($responseKeys["success"]) !== 1 || $responseKeys["score"] < 0.5) {
-        echo "Please complete the CAPTCHA.";
-        exit;
-    }
-
     $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
     $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
     $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
