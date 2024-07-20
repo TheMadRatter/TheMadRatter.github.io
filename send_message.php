e6abd89e580e202c3f1e9f6b21f6016d5809a1cf
TheMadRatter <50557502+TheMadRatter@users.noreply.github.com>
2024-07-19 21:57:21 -0400

    Update send_message.php
    captcha

--- send_message.php
+++ send_message.php
@@ -4,6 +4,18 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verify reCAPTCHA
+    $recaptcha_secret = "YOUR_SECRET_KEY";
+    $recaptcha_response = $_POST['g-recaptcha-response'];
+
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response");
+    $responseKeys = json_decode($response, true);
+
+    if (intval($responseKeys["success"]) !== 1) {
+        echo "Please complete the CAPTCHA.";
+        exit;
+    }
+
     $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
     $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
     $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
